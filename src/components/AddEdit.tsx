fe2c08d9a25f134d377835212fd81bd9e34b7861
aliasghargill <stylinalivlogs@gmail.com>
2024-01-04 23:33:55 +0500

    Inital Changes

--- src/components/AddEdit.tsx
+++ src/components/AddEdit.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User } from "../models/studennt.model";
 import { useAddUserMutation, useGetUsersQuery, useEditUserQuery } from "../features/userDetailApi";
 import { useNavigate, useParams } from "react-router-dom";
@@ -6,7 +6,7 @@ import { useNavigate, useParams } from "react-router-dom";
 const AddEdit = () => {
     const navigate = useNavigate()
     const [users, setUsers] = useState<User>(Object);
-    // const [editMode, setEditMode] = useNavigate<boolean>(false);
+    const [editMode, setEditMode] = useState<boolean>(false);
     const [addUser] = useAddUserMutation();
     const { refetch } = useGetUsersQuery();
     const { id } = useParams();
@@ -24,7 +24,14 @@ const AddEdit = () => {
         refetch();
         // console.log(users);
     }
-
+    useEffect(() => {
+        if (id && data) {
+            setEditMode(true);
+            setUsers({ ...data });
+        } else {
+            setEditMode(false);
+        }
+    }, [id, data]);
 
     return (
         <>
@@ -35,19 +42,19 @@ const AddEdit = () => {
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                             Username
                         </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="name" id="username" type="text" onChange={handleChange} placeholder="Username" />
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="name" id="username" type="text" onChange={handleChange} placeholder="Username" value={users?.name || ""} />
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
                             Email
                         </label>
-                        <input onChange={handleChange} className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="email" id="email" type="email" placeholder="Email" />
+                        <input onChange={handleChange} className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="email" id="email" type="email" placeholder="Email" value={users?.email || ""} />
                     </div>
-                    <div className="">
+                    {/* <div className="">
                         <label className="block text-sm  text-gray-700 font-bold " htmlFor="user_avatar">Upload Image</label>
-                        <input onChange={handleChange} className="block w-full text-sm text-gray-700 p-1  border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" name="pic" />
+                        <input onChange={handleChange} className="block w-full text-sm text-gray-700 p-1  border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" name="pic" value={users?.pic || ""} />
                         <div className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A profile picture is useful to confirm your are logged into your account</div>
-                    </div>
+                    </div> */}
                     <div className="flex items-center justify-between">
                         <button className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none  w-full focus:shadow-outline" type="submit">
                             Add Me
